b6e40af233e614d3576443d43aee14f865693d04
Isuru Nayanajith <96404302+Isurunayana@users.noreply.github.com>
2023-09-18 23:34:05 +0530

    change app bar

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,25 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
       ),
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text(""),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.face),
+              color: Colors.black,
+              hoverColor: Colors.blue,
+              onPressed: () {},
+            ),
+          ],
+          backgroundColor:Color(0xFF8DC63F),
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            color: Colors.black,
+            hoverColor: Colors.blue,
+              onPressed: () {},
+          ),
+        ),
         body: ListView(children: [
           CanteenProcess(),
           OrderValidationForm(),
